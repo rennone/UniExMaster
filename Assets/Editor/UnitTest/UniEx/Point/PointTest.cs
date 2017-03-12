e66de3df9442099d927ec6ae5a2c071ed4daafd3
Ren <dotabatadjango@gmail.com>
2017-03-12 22:23:26 +0900

    UniExのテストフォルダを追加

---
+++ Assets/Editor/UnitTest/UniEx/Point/PointTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UniEx;
+using UnityEditor.VersionControl;
+
+namespace UniEx.Test
+{
+    public static class PointTest
+    {
+
+        [Test]
+        public static void ConvertTest()
+        {
+            var p = new Point3<int>(1, 2, 3);
+
+            Debug.Log(p.Float / 2f);
+        }
+
+        [Test]
+        public static void OperatorTest()
+        {
+            var a = new Point2<int>(1, 2);
+            var b = new Point2<int>(4, 5);
+
+            Assert.IsTrue((a + b) == new Point2<int>(5, 7), "Add");
+            Assert.IsTrue((a - b) == new Point2<int>(-3, -3), "Subtract");
+            Assert.IsTrue(a * 3 == new Point2<int>(3, 6), "Multiply");
+            Assert.IsTrue(a / 2 == new Point2<int>(0, 1), "Divide");
+
+            var c = new Point3<int>(1, 2, 3);
+            var d = new Point3<int>(4, 5, 6);
+
+            Assert.IsTrue((c + d) == new Point3<int>(5, 7, 9), "Add");
+            Assert.IsTrue((c - d) == new Point3<int>(-3, -3, -3), "Subtract");
+            Assert.IsTrue(c * 3 == new Point3<int>(3, 6, 9), "Multiply");
+            Assert.IsTrue(c / 2 == new Point3<int>(0, 1, 1), "Divide");
+        }
+    }
+}
+
---
+++ Assets/Editor/UnitTest/UniEx/UnityEngineEx/VectorExTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UniEx;
+using UnityEngine;
+
+namespace UniEx.Test
+{
+    public class VectorExTest : MonoBehaviour
+    {
+
+        [Test]
+        public static void PutTest()
+        {
+            Vector3 hoge = new Vector3(1, 2, 3);
+            Debug.Log(hoge + " : " + hoge.PutX(10));
+            Debug.Log(hoge + " : " + hoge.PutY(10));
+            Debug.Log(hoge + " : " + hoge.PutZ(10));
+        }
+    }
+}
